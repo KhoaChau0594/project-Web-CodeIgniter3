450e02d215b2340ed55fed1f693e1b3245f5fa61
KhoaChau0594 <43965503+KhoaChau0594@users.noreply.github.com>
2019-06-27 17:59:39 +0700

    Add files via upload

---
+++ footer/footer.php
@@ -0,0 +1,15 @@
+
+	<div class="footer page-footer">
+		<div class="container-fluid footer-upper">
+			<ul>
+				<li><i class="fas fa-phone"></i> 0263 3825 444</li>
+				<li><i class="fas fa-map-marker-alt"></i> 2 Trần Phú, Phường 3, Tp. Đà Lạt, Phường 3</li>
+			</ul>
+		</div>
+		<div class="container-fluid footer-center">
+			<ul>
+				<li>Trường Đại học Công nghệ Thông tin ĐHQG TP.HCM</li>
+				<li>Đồ án trang web quản lý khách sạn</li>
+			</ul>
+		</div>
+	</div>
